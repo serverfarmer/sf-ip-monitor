ad642e51e257c983079df1dfa7b7affea69ff2a4
Tomasz Klim <github@tomaszklim.pl>
2015-12-24 20:37:45 +0100

    added network state report script

---
+++ cron/report.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+. /opt/farm/scripts/functions.custom
+# send periodic (eg. daily) detailed reports about local and external network
+# state; useful on servers with dynamic local and/or remote IP address
+# Tomasz Klim, 2009-2011, Dec 2015
+
+
+provider="wppl"
+
+
+if [ "$1" != "" ]; then
+	rcpt="$1"
+else
+	rcpt="network-reports@`external_domain`"
+fi
+
+if [ "$2" != "" ]; then
+	template="$2"
+else
+	template="Network state on %%host%%"
+fi
+
+host=`hostname`
+subject=`echo "$template" |sed s/%%host%%/$host/g`
+
+(
+
+echo -n "External IP: "
+/opt/sf-ip-monitor/providers/$provider.sh
+echo
+echo
+/sbin/ifconfig -a |grep -v 127.0.0 |grep -B1 "inet addr"
+echo
+echo
+/sbin/route -ne
+echo
+echo
+/usr/sbin/arp -na
+
+) |mail -s "$subject" $rcpt
--- setup.sh
+++ setup.sh
@@ -13,3 +13,7 @@ if ! grep -q /opt/sf-ip-monitor/cron/check.sh /etc/crontab && [ "$IP" != "" ]; t
 	sed -i -e "/check-external-ip.sh/d" /etc/crontab
 	echo "*/5 * * * * root /opt/sf-ip-monitor/cron/check.sh $IP" >>/etc/crontab
 fi
+
+if ! grep -q /opt/sf-ip-monitor/cron/report.sh /etc/crontab && [ -d /etc/NetworkManager ]; then
+	echo "34 18 * * * root /opt/sf-ip-monitor/cron/report.sh $IP" >>/etc/crontab
+fi
