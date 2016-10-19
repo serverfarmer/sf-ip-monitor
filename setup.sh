a9c31125ad8e48c72ba4bd0e87b5a4b38d1fd087
Tomasz Klim <github@tomaszklim.pl>
2016-10-19 23:55:08 +0200

    removed old transitional code

--- setup.sh
+++ setup.sh
@@ -8,7 +8,6 @@ provider="wppl"
 IP=`/opt/farm/ext/ip-monitor/providers/$provider.sh`
 
 if ! grep -q /opt/farm/ext/ip-monitor/cron/check.sh /etc/crontab && [ "$IP" != "" ]; then
-	sed -i -e "/check-external-ip.sh/d" /etc/crontab
 	echo "*/5 * * * * root /opt/farm/ext/ip-monitor/cron/check.sh $IP" >>/etc/crontab
 fi
 
