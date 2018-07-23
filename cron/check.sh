05f6da69b5f39f4f84bce77671fdb932ffe3af0e
Tomasz Klim <github@tomaszklim.pl>
2018-07-23 19:29:36 +0200

    ask for individual phone number during setup

--- cron/check.sh
+++ cron/check.sh
@@ -4,9 +4,6 @@
 # Tomasz Klim, Sep 2013, Mar 2015, Dec 2015, Jul 2018
 
 
-phone="`notify_phone`"
-email="external-ip@`external_domain`"
-
 file="/var/cache/cacti/external-ip"
 current="/etc/local/.config/external.ip"
 
@@ -17,7 +14,12 @@ if [ ! -s $file.new ]; then exit; fi
 # if current IP differs from previous one, trigger notifications
 diff -u $current $file.new >$file.diff
 if [ -s $file.diff ]; then
-	/opt/farm/ext/sms-smsapi/sms.sh $phone "`hostname` detected external IP change to `cat $file.new`" >>$file.diff
-	cat $file.diff |mail -s "External IP change [`hostname`]" $email
+
+	if [ -s /etc/local/.config/notify.phone ]; then
+		phone="`cat /etc/local/.config/notify.phone`"
+		/opt/farm/ext/sms-smsapi/sms.sh $phone "`hostname` detected external IP change to `cat $file.new`" >>$file.diff
+	fi
+
+	cat $file.diff |mail -s "External IP change [`hostname`]" "external-ip@`external_domain`"
 	mv -f $file.new $current
 fi
--- setup.sh
+++ setup.sh
@@ -1,13 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 
 /opt/farm/scripts/setup/extension.sh sf-cache-utils
 /opt/farm/scripts/setup/extension.sh sf-net-utils
-/opt/farm/scripts/setup/extension.sh sf-sms-smsapi
 
+phone="/etc/local/.config/notify.phone"
 current="/etc/local/.config/external.ip"
 
 if [ ! -f $current ]; then
 	/opt/farm/ext/net-utils/external/ip.sh >$current
+fi
+
+if [ ! -f $phone ]; then
+	if [ "$PHONE_NUMBER" = "" ]; then
+		read -p "phone number for admin notifications (using smsapi.pl) - 9 digits, or empty to disable notifications: " PHONE_NUMBER
+	fi
+
+	if [ "$PHONE_NUMBER" != "" ]; then
+		if [ ${#PHONE_NUMBER} != 9 ]; then
+			echo "error: invalid phone number length, aborting"
+			exit 1
+		elif ! [[ $PHONE_NUMBER =~ ^[0-9]+$ ]]; then
+			echo "error: phone number contains invalid characters, aborting"
+			exit 1
+		fi
+	fi
+
+	echo -n "$PHONE_NUMBER" >$phone
+fi
+
+if [ -s $phone ]; then
+	/opt/farm/scripts/setup/extension.sh sf-sms-smsapi
 fi
 
 if ! grep -q /opt/farm/ext/ip-monitor/cron/check.sh /etc/crontab; then
